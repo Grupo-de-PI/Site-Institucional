64f048ce28786e103cb4728076124f169e2fcfe9
Davi Silva <davi.fsilva@sptech.school>
2025-06-04 21:41:20 -0300

    fix: atualizar versionamento conflituoso com novas melhorias

--- AirSafe/Site-Institucional/src/controllers/graficoController.js
+++ AirSafe/Site-Institucional/src/controllers/graficoController.js
@@ -1,7 +1,7 @@
 var graficoModel = require("../models/graficoModel");
 
 function listarLinha(req, res) {
-    console.log("1 - Cheguei no controller da linha, vamoo!");
+    // console.log("1 - Cheguei no controller da linha, vamoo!");
     graficoModel.listarLinha().then(
         function (resultado) {
             res.status(200).json(resultado);
@@ -14,25 +14,12 @@ function listarLinha(req, res) {
     )
 }
 
-function listarNovasLinhas(req, res) {
-    console.log("1 - Cheguei no controller da nova linha, vamoo!");
-    graficoModel.listarNovasLinhas().then(
-        function (resultado) {
-            res.status(200).json(resultado);
-        }
-    ).catch(
-        function (erro) {
-            console.log("1 - Deu merda aqui no controller da nova linha: /n");
-            res.status(500).json({ erro: erro.sqlMessage || erro.message || erro });
-        }
-    )
-}
 
 function listarBarra(req, res) {
-    console.log("2 - Cheguei no controller da barra, vamoo!");
+    // console.log("2 - Cheguei no controller da barra, vamoo!");
     graficoModel.listarBarra().then(
         function (resultado) {
-            console.log("2 - To na função do controller da barra");
+            // console.log("2 - To na função do controller da barra");
             res.status(200).json(resultado);
         }
     ).catch(
@@ -45,6 +32,5 @@ function listarBarra(req, res) {
 
 module.exports = {
     listarLinha,
-    listarBarra,
-    listarNovasLinhas
+    listarBarra
 }
--- AirSafe/Site-Institucional/src/routes/graficos.js
+++ AirSafe/Site-Institucional/src/routes/graficos.js
@@ -4,20 +4,16 @@ var router = express.Router();
 var graficoController = require("../controllers/graficoController");
 
 router.get("/linha", function (req,res) {
-    console.log("1 - Cheguei aqui no route da linha");
+    // console.log("1 - Cheguei aqui no route da linha");
     // graficos/linha
     graficoController.listarLinha(req, res);
 } );
 
 router.get("/barra", function (req,res) {
-    console.log("2 - Cheguei aqui no route da barra");
+    // console.log("2 - Cheguei aqui no route da barra");
     // graficos/barra
     graficoController.listarBarra(req, res);
 } );
 
-router.get("/novasLinhas", function (req,res) {
-    console.log("1 - Cheguei aqui no route das novas linhas");
-    // graficos/barra
-    graficoController.listarNovasLinhas(req, res);
-})
+
 module.exports = router;
